596ae1c8918c4f9d1be64efcb2e98b4c95cbbc7e
psm9718@gmail.com <psm9718@gmail.com>
2023-05-04 19:57:10 +0900

    test: change test api to RequestMapping

--- src/main/java/com/msedonald/controller/GameController.java
+++ src/main/java/com/msedonald/controller/GameController.java
@@ -1,12 +1,12 @@
 package com.msedonald.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
 
-    @GetMapping("/api/test")
+    @RequestMapping("/api/test")
     public String startGame() {
         return "test api";
     }
