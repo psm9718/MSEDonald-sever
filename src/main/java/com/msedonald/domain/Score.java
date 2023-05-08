48a6c6fad3fbfca357404d46694782e1cd8d4658
psm9718@gmail.com <psm9718@gmail.com>
2023-05-08 22:25:38 +0900

    feat: make Score Domain

---
+++ src/main/java/com/msedonald/domain/Score.java
@@ -0,0 +1,18 @@
+package com.msedonald.domain;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Table(name = "scores")
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
