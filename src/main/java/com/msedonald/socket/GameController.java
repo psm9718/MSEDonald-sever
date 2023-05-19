243ada17ec8fe51d3ef482524f9f158358c56192
psm9718@gmail.com <psm9718@gmail.com>
2023-05-19 14:55:04 +0900

    test: test api log

--- src/main/java/com/msedonald/socket/GameController.java
+++ src/main/java/com/msedonald/socket/GameController.java
@@ -2,17 +2,20 @@ package com.msedonald.socket;
 
 import com.msedonald.socket.data.MessageDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GameController {
 
     @GetMapping("/api/test")
     public String startGame() {
+        log.info(">> test api received");
         return "test api";
     }
 
